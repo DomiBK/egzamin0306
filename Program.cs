4e9d2a313b281318d003afc92b931a2fc3f1cee5
DomiBK <148908251+DomiBK@users.noreply.github.com>
2025-12-09 10:12:12 +0100

    Add Film class usage in Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using egzamin0306;
+
+
+
+Film film = new Film();
+Console.WriteLine(film);
+film.Tytul = "kapitan bobmba";
+Console.WriteLine(film.Tytul);
+film.Tytul = "kapitan bobmba w kosmosie";
+Console.WriteLine(film.Liczbawypozyczen);
+film.RealizacjaWypozyczen();
+Console.WriteLine(film.Liczbawypozyczen);
